8dc24bbab5e535f09e59e3d72bcdfb8e39e98581
Rafiul <rafiul13062001@gmail.com>
2023-05-08 21:27:05 +0600

    Brick Breaker

---
+++ src/BrickGame/BrickGameFrame.java
@@ -0,0 +1,33 @@
+package BrickGame;
+
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.JFrame;
+
+public final class BrickGameFrame extends JFrame {
+
+    private final int SCREEN_WIDTH = 800;
+    private final int SCREEN_HEIGHT = 600;
+
+    public BrickGameFrame() {
+        setVisible(true);
+        setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setTitle("! ! BRICK GAME ! !");
+        Image imageIcon = Toolkit.getDefaultToolkit().getImage("src//BrickGame//BrickLogo.png");
+        setIconImage(imageIcon);
+        setResizable(false);
+        BrickGamePanel panel = new BrickGamePanel();
+        panel.setLayout(null);
+        add(panel);
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosed(WindowEvent e) {
+                panel.setTimerStop();
+            }
+        });
+    }
+}
